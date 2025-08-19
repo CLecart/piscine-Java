86067b3a9a90053825b72558591c82d20bd17a12
clecart <djlike@hotmail.fr>
2025-08-19 11:25:55 +0200

    test exerciserunner

---
+++ Palindrome/Palindrome.java
@@ -0,0 +1,20 @@
+public class Palindrome {
+    public static boolean isPalindrome(String s) {
+        if (s == null || s.isEmpty()) {
+            return true;
+        }
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
