259a5d5c08b3d0a5fb06364a84b6318939fdea3b
clecart <djlike@hotmail.fr>
2025-08-29 09:35:38 +0200

    test sortlist

---
+++ SortList/SortList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SortList {
+
+    public static List<Integer> sort(List<Integer> list) {
+        List<Integer> sortedList = new ArrayList<>(list);
+        Collections.sort(sortedList);
+        return sortedList;
+    }
+    public static List<Integer> sortReverse(List<Integer> list) {
+        List<Integer> sortedList = new ArrayList<>(list);
+        Collections.sort(sortedList, Collections.reverseOrder());
+        return sortedList;
+    }
+}
+
+
