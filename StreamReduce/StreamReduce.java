e7d960286e01f643e546d8dfc248cc88c68349c3
clecart <djlike@hotmail.fr>
2025-08-29 13:30:24 +0200

    test streamreduce

---
+++ StreamReduce/StreamReduce.java
@@ -0,0 +1,18 @@
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class StreamReduce {
+    public static Integer sumAll(Stream<Integer> s) {
+        if (s == null) return 0;
+        return s.filter(Objects::nonNull)
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    public static Integer divideAndAddElements(Stream<Integer> s, int divider) {
+        if (s == null || divider == 0) return 0;
+        return s.filter(Objects::nonNull)
+                .mapToInt(i -> i / divider)
+                .sum();
+    }
+}
