472f9d6b647e50d33755fceaef80e653f802ef05
clecart <djlike@hotmail.fr>
2025-08-29 09:43:35 +0200

    test32 sortlist

--- SortList/SortList.java
+++ SortList/SortList.java
@@ -5,16 +5,16 @@ import java.util.List;
 public class SortList {
 
     public static List<Integer> sort(List<Integer> list) {
-        if (list == null || list.isEmpty()) {
-            return Collections.emptyList();
+        if (list == null) {
+            return null;
         }
         List<Integer> sortedList = new ArrayList<>(list);
         Collections.sort(sortedList);
         return sortedList;
     }
     public static List<Integer> sortReverse(List<Integer> list) {
-        if (list == null || list.isEmpty()) {
-            return Collections.emptyList();
+        if (list == null) {
+            return null;
         }
         List<Integer> sortedList = new ArrayList<>(list);
         Collections.sort(sortedList, Collections.reverseOrder());
