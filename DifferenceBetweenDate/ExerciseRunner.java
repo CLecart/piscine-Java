635b3170e340d596471ab6ee210b5456d5e450d7
clecart <djlike@hotmail.fr>
2025-09-01 16:00:11 +0200

    test3 differencebetweendate

--- DifferenceBetweenDate/ExerciseRunner.java
+++ DifferenceBetweenDate/ExerciseRunner.java
@@ -1,29 +1,14 @@
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.Period;
 
-public class DifferenceBetweenDate {
+public class ExerciseRunner {
 
-    public static Duration durationBetweenTime(LocalTime localTime1, LocalTime localTime2) {
-        if (localTime1 == null || localTime2 == null) return null;
-        Duration d = Duration.between(localTime1, localTime2);
-        return d.isNegative() ? d.negated() : d;
-    }
-
-    public static Period periodBetweenDate(LocalDate date1, LocalDate date2) {
-        if (date1 == null || date2 == null) return null;
-        if (date1.isAfter(date2)) {
-            return Period.between(date2, date1);
-        } else {
-            return Period.between(date1, date2);
-        }
-    }
-
-    public static Long numberOfHoursBetweenDateTime(LocalDateTime dateTime1, LocalDateTime dateTime2) {
-        if (dateTime1 == null || dateTime2 == null) return null;
-        Duration d = Duration.between(dateTime1, dateTime2);
-        return Math.abs(d.toHours());
+    public static void main(String[] args) {
+        Duration duration = DifferenceBetweenDate.durationBetweenTime(LocalTime.of(12, 54, 32), LocalTime.of(21, 23, 53));
+        System.out.println(duration.toHoursPart() + "H" + duration.toMinutesPart() + "M" + duration.toSecondsPart() + "S");
+        Period period = DifferenceBetweenDate.periodBetweenDate(LocalDate.of(2020, 10, 13), LocalDate.of(2022, 5, 8));
+        System.out.println(period.getYears() + "Y" + period.getMonths() + "M" + period.getDays() + "D");
+        System.out.println(DifferenceBetweenDate.numberOfHoursBetweenDateTime(LocalDateTime.of(2022, 4, 12, 16, 18, 56), LocalDateTime.of(2022, 5, 10, 21, 54, 56)));
     }
 }
