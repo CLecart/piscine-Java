8ee5bc89d8b33644391b9d26a265177c5b4c1c11
clecart <djlike@hotmail.fr>
2025-09-01 14:09:11 +0200

    testregexreplace

---
+++ RegexReplace/RegexReplace.java
@@ -0,0 +1,12 @@
+
+public class RegexReplace {
+    public static String removeUnits(String s) {
+        if (s == null) return null;
+        return s.replaceAll("\\s*(cm|€)\\b", "");
+    }
+
+    public static String obfuscateEmail(String s) {
+        if (s == null) return null;
+        return s.replaceAll("([a-zA-Z0-9._%+-])([a-zA-Z0-9._%+-]*)(@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,})", "$1***$3");
+    }
+}
